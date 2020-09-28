8fe187d99b895007df70d1c6793190133e4676ee
Sholde <exshode@gmail.com>
2020-09-28 21:56:13 +0200

    Change static pointers to dynamic pointers and Add print function for matrix

--- 5.0/main.c
+++ 5.0/main.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
 #include <stdlib.h>
+
+//Print matrix on standard output
+void print_matrix(int n, double **m)
+{
+  for(int i = 0; i < n; i++)
+    {
+      printf("| ");
+      for(int j = 0; j < n; j++)
+	{
+	  printf("%lf ", m[i][j]);
+	}
+      printf("|\n");
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -7,8 +22,17 @@ int main(int argc, char **argv)
   
   int n = atoi(argv[1]);
 
-  double a[n][n], b[n][n], c[n][n];
+  double **a = malloc(sizeof(double *) * n);
+  double **b = malloc(sizeof(double *) * n);
+  double **c = malloc(sizeof(double *) * n);
 
+  for(int i = 0; i < n; i++)
+    {
+      a[i] = malloc(sizeof(double) * n);
+      b[i] = malloc(sizeof(double) * n);
+      c[i] = malloc(sizeof(double) * n);
+    }
+  
   for(int i = 0; i < n; i++)
     {
       for(int j = 0; j < n; j++)
@@ -19,6 +43,18 @@ int main(int argc, char **argv)
 	    }
 	}
     }
+
+  print_matrix(n, c);
+
+  for(int i = 0; i < n; i++)
+    {
+      free(a[i]);
+      free(b[i]);
+      free(c[i]);
+    }
+  free(a);
+  free(b);
+  free(c);
   
   return 0;
 }
